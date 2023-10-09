c46ff4be72813e412322590eeef340688c64896c
musta55 <bsse1114@iit.du.ac.bd>
2023-10-10 01:43:41 +0600

    dot file generated

--- python_output.py
+++ python_output.py
@@ -71,7 +71,7 @@ def my_tracer(frame, event, arg=None):
         print('The source code is:\n')
         print_executed_code(tracer_function_code)
 
-        cfg = CFGBuilder().build_from_file("dot", "lcs.py")
+        cfg = CFGBuilder().build_from_file("dot", "call.py")
         cfg.build_visual('dot', 'png')
         
         print('''
@@ -350,7 +350,7 @@ def htmlInit():
     sys.stdout = f
 
     # Initializes the webpage along with the CSS
-    # NOTE: lcs.py is replaced with the name of the file by my-second-page.js
+    # NOTE: call.py is replaced with the name of the file by my-second-page.js
     print('''
 		<!DOCTYPE html>
 		<html>
@@ -521,7 +521,7 @@ def htmlInit():
 		<body>
 		<div class="w3-container">
 
-		<h2>Filename : lcs.py</h2>
+		<h2>Filename : call.py</h2>
     <hr>
 		<p>Open and collapse the accordian to see the summary</p>
 	''')
@@ -533,41 +533,22 @@ htmlInit()
 settrace(my_tracer)
 
 # <__b_s__> is replaced with the code selected by the user by my-second-page.js
-def longest_common_subsequence(X, Y):
-    m, n = len(X), len(Y)
-    
-    # Create a table to store the length of LCS for subproblems
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
-    
-    # Fill in the DP table using a bottom-up approach
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if X[i - 1] == Y[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-    
-    # Reconstruct the LCS from the DP table
-    lcs = []
-    i, j = m, n
-    while i > 0 and j > 0:
-        if X[i - 1] == Y[j - 1]:
-            lcs.append(X[i - 1])
-            i -= 1
-            j -= 1
-        elif dp[i - 1][j] > dp[i][j - 1]:
-            i -= 1
-        else:
-            j -= 1
-    
-    lcs.reverse()
-    return "".join(lcs)
+# Function to calculate the square of a number
+def calculate_square(x):
+    return x ** 2
 
-# Test the longest_common_subsequence function
-X = "AGGTAB"
-Y = "GXTXAYB"
-result = longest_common_subsequence(X, Y)
-print(f"The Longest Common Subsequence is '{result}'.")
+def main():
+    # Test input
+    number = 5
+
+    # Call the calculate_square function
+    result = calculate_square(number)
+
+    # Display the result
+    print(f"The square of {number} is {result}.")
+
+if __name__ == "__main__":
+    main()
 
 
 # Tracer function is set to None
--- quick_sort.py
+++ quick_sort.py
@@ -1,16 +1,5 @@
 import inspect
 def quick_sort(collection: list) -> list:
-    """A pure Python implementation of quick sort algorithm
-    :param collection: a mutable collection of comparable items
-    :return: the same collection ordered by ascending
-    Examples:
-    >>> quick_sort([0, 5, 3, 2, 2])
-    [0, 2, 2, 3, 5]
-    >>> quick_sort([])
-    []
-    >>> quick_sort([-2, 5, 0, -45])
-    [-45, -2, 0, 5]
-    """
     if len(collection) < 2:
         return collection
     pivot = collection.pop()  # Use the last element as the first pivot
