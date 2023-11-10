17788f2ea60c3ff6b59f6a75a8fba807964db5c1
musta55 <bsse1114@iit.du.ac.bd>
2023-11-10 22:49:12 +0600

    updates extension

--- pytracex/extension.js
+++ pytracex/extension.js
@@ -1,7 +1,9 @@
 const vscode = require('vscode');
 const fs = require('fs');
 const path = require('path');
+const { SourceTextModule } = require('vm');
 
+// Function to analyze a Python file
 function analyzeFile(filePath) {
   // Read the content of the selected file
   const fileContent = fs.readFileSync(filePath, 'utf-8');
@@ -19,10 +21,11 @@ function analyzeFile(filePath) {
   const pythonScriptPath = path.join(__dirname, 'python_output.py');
   fs.writeFileSync(pythonScriptPath, pythonCode, 'utf-8');
 
-  // Open the generated HTML file in a separate window using "Live Server"
+  // Open the generated HTML file with Live Server
   const webpagePath = path.join(__dirname, 'pytracex.html');
   openWithLiveServer(webpagePath);
 }
+
 
 function openWithLiveServer(webpagePath) {
   // Open the HTML file with Live Server in a separate window
@@ -38,6 +41,8 @@ function openWithLiveServer(webpagePath) {
   } else {
     vscode.window.showErrorMessage('Live Server extension is not installed.');
   }
+
+  console.log("Open live server function")
 }
 
 vscode.commands.registerCommand('pytracex.openHtmlPreview', async (uri) => {
@@ -53,6 +58,14 @@ vscode.commands.registerCommand('pytracex.openHtmlPreview', async (uri) => {
   webViewPanel.reveal();
 });
 
+// Command to analyze a Python file
+vscode.commands.registerCommand('pytracex.analyzePythonFile', async (uri) => {
+  if (uri && uri.fsPath) {
+    analyzeFile(uri.fsPath);
+  } else {
+    vscode.window.showErrorMessage('No Python file selected for analysis.');
+  }
+});
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -60,7 +73,7 @@ function activate(context) {
   console.log('Congratulations, your extension "pytracex" is now active!');
 
   let disposable = vscode.commands.registerCommand('pytracex.helloWorld', function () {
-    const filePathToExamine = 'F:/8th Semester/SPL3/driver-spl3/pytracex/Calc_profit.py';
+    const filePathToExamine = 'F:/8th Semester/SPL3/driver-spl3/pytracex/Fibonacci.py';
     analyzeFile(filePathToExamine);
 
     vscode.window.showInformationMessage('Hello World from Musta!');
